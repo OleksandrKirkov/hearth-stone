7c2005570371b7491098d2600f70214e554b1756
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-09-20 19:20:01 +0300

    chore: configure project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    //output: 'export',
+    //basePath: '/health_stone',
+    //assetPrefix: '/health_stone/',
+    images: {
+        unoptimized: false
+    }
+};
 
 export default nextConfig;
