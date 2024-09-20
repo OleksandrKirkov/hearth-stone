574aac3bddf04ef4352745199e4fa5368fd4dad0
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-09-21 00:59:29 +0300

    feat(Card.tsx): character id replaced by data

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,34 +1,17 @@
-import { FC, useCallback, useEffect, useState } from 'react'
+import { FC } from 'react'
 import styles from './Card.module.css'
-import { getCharacter } from '@/services/Api'
 import { CharacterType } from '@/types/character.type'
 
 
-const Card: FC<{ id: number }> = ({ id }) => {
-    const [characterState, setScharacterState] = useState<CharacterType | null>(null)
-
-    const fetchCharacter = useCallback(async () => {
-        const character = await getCharacter(String(id));
-
-        if (!character.error)
-            setScharacterState(character.data)
-    }, [])
-
-    useEffect(() => {
-        fetchCharacter()
-    }, [fetchCharacter])
-
+const Card: FC<{ data: CharacterType }> = ({ data }) => {
     return <div>
-        {characterState
-            ? <div className={styles.card}>
-                <div className={styles.wrapper}>
-                    <div className={styles.image}>
-                        <img src={characterState.image.url} alt='card' />
-                    </div>
+        <div className={styles.card}>
+            <div className={styles.wrapper}>
+                <div className={styles.image}>
+                    <img src={data.image.url} alt='card' />
                 </div>
-            </div> : <div>
-                error
-            </div>}
+            </div>
+        </div>
     </div>
 }
 
