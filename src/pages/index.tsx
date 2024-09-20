d49ccf370966b12f5965bdd3a3d8bf272c3c9e88
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-09-21 02:20:40 +0300

    feat(index.tsx): add enemy and update styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
 import Cart from "@/components/card/Card"
 import { NextPageWithLayout } from "./_app"
 import styles from './Home.module.css'
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import useDeck from "@/hooks/useDeck"
 import { PlayerType } from "@/types/player.type"
 import { initialPlayer } from "@/assets/store/reducers/playerSlice"
 import { startGame } from "@/assets/store/reducers/gameSlice"
 import { RootState } from "@/assets/store/store"
-import { CharacterType } from "@/types/character.type"
+import { initialEnemy } from "@/assets/store/reducers/enemySlice"
 
 const Main: NextPageWithLayout = () => {
-    const [ enemyState, setEnemyState ] = useState<CharacterType[]>([])
-
     const player = useSelector((state: RootState) => state.player)
+    const enemy = useSelector((state: RootState) => state.enemy)
 
     const dispatch = useDispatch()
     const { getDeck } = useDeck()
@@ -31,31 +30,40 @@ const Main: NextPageWithLayout = () => {
             }
 
             dispatch(initialPlayer(playerData))
-            dispatch(startGame())
         }
 
         const fetchEnemyDeck = async () => {
             const deck = await getDeck()
 
-            setEnemyState(deck)
+            const enemyData: PlayerType = {
+                id: 1,
+                name: 'Enemy',
+                hero: 'Hero',
+                deck: deck,
+                mana: 0
+            }
+
+            dispatch(initialEnemy(enemyData))
         }
 
         fetchPlayerDeck();
         fetchEnemyDeck();
+
+        dispatch(startGame())
     }, [])
 
     return <div className={styles.wrapper}>
         <div className={styles.section}>
-            <div className={styles.mana}>1</div>
+            <div className={styles.mana}>{enemy.mana}</div>
             <div className={styles.deck}>
-                {enemyState.map((card, index) => (
+                {enemy.deck.map((card, index) => (
                     <Cart key={index} data={card} />
                 ))}
             </div>
             <div className={styles.hero}>3</div>
         </div>
         <div className={styles.section}>
-            <div className={styles.mane}>1</div>
+            <div className={styles.mana}>{enemy.mana}</div>
             <div className={styles.deck}>
                 {player.deck.map((card, index) => (
                     <Cart key={index} data={card} />
