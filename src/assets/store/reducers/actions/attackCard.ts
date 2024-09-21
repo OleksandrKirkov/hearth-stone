187bfed58a3c9f01f22e112a05dc9e39a2c443b7
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-09-22 00:13:00 +0300

    feat: add attack card fucntion

---
+++ src/assets/store/reducers/actions/attackCard.ts
@@ -0,0 +1,48 @@
+import { TURN_STATUS } from "@/types/game.type";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
+import { CardType } from "@/types/card.type";
+
+interface IAttack {
+    turnStatus: TURN_STATUS
+    attackCardId: number
+    targetCardId: number
+}
+
+export function getCardById(cardId: number, deck: CardType[]) {
+    return deck.find((card) => card.id === cardId)
+}
+
+export function attackCard(state: RootState, action: PayloadAction<{ data: IAttack }>) {
+    const { turnStatus, attackCardId, targetCardId } = action.payload.data
+
+    const isAttackPlayer = turnStatus === TURN_STATUS.player
+
+    const attackerCard = getCardById(
+        attackCardId,
+        isAttackPlayer ? state.player.deck : state.enemy.deck
+    )
+
+    const targetCard = getCardById(
+        targetCardId,
+        isAttackPlayer ? state.player.deck : state.enemy.deck
+    )
+
+    if (attackerCard && targetCard && attackerCard.isAttack) {
+        targetCard.health -= attackerCard.attack
+        attackerCard.isAttack = false
+        attackerCard.attackPerTurn -= 1
+
+        if(targetCard.health <= 0) {
+            if(isAttackPlayer) {
+                state.enemy.deck = state.enemy.deck.filter(
+                    (card) => card.id !== targetCardId
+                )
+            } else {
+                state.player.deck = state.enemy.deck.filter(
+                    (card) => card.id !== targetCardId
+                )
+            }
+       }
+    }
+}
