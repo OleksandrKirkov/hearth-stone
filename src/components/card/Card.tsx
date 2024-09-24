1e6fdd822727dd65682eb3e7daed2ef7fa9d9865
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-09-25 02:19:56 +0300

    feat(Card.tsx): add props interface

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,9 +2,14 @@ import { FC } from 'react'
 import styles from './Card.module.css'
 import { CardType } from '@/types/card.type'
 
+interface ICard {
+    card: CardType
+    onClick?: () => void
+    enemy?: boolean
+}
 
-const Card: FC<{ card: CardType, enemy: boolean }> = ({ card, enemy = false }) => {
-    return <div>
+const Card: FC<ICard> = ({ card, onClick, enemy = false }) => {
+    return <div onClick={onClick}>
         <div className={styles.card}>
             <div className={styles.wrapper}>
                 <div className={styles.image}>
