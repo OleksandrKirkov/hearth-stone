402221ae1bdaa34a2ee21b6c2e64a976b8cfd634
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-10-09 02:24:27 +0300

    feat: add player component

---
+++ src/pages/player/Player.tsx
@@ -0,0 +1,61 @@
+import { FC, useState } from "react";
+import styles from "./Player.module.css"
+import Image from "next/image";
+import { useAppSelector } from "@/hooks/useRedux";
+import { RootState } from "@/assets/store/store";
+import Card from "@/components/card/Card";
+import { TURN_STATUS } from "@/types/game.type";
+import useGame from "@/hooks/useGame";
+
+
+const Player: FC = () => {
+
+    const player = useAppSelector((state: RootState) => state.player)
+    const game = useAppSelector((state: RootState) => state.game)
+
+    const [attackCardId, setAttackCardId] = useState<number | null>()
+
+    const { startGame, attackCard, playCard } = useGame()
+
+    const onAttckerHandler = (id: number) => {
+        if (game.currentTurn !== TURN_STATUS.player) return
+
+        setAttackCardId(id)
+
+        playCard(id)
+    }
+
+    return (
+        <div className={styles.player}>
+            <div className={styles.table}>
+                {player.deck.filter((card) => card.isDeck === false).map((card, index) => (
+                    <Card
+                        key={index}
+                        card={card}
+                        enemy={false}
+                    />
+                ))}
+            </div>
+            <div className={styles.interface}>
+                <div className={styles.health}>
+                    <Image src="/health.png" alt="health" objectFit="contain" fill={true} />
+                </div>
+                <div className={styles.deck}>
+                    {player.deck.filter((card) => card.isDeck == true).map((card, index) => (
+                        <Card
+                            onClick={() => onAttckerHandler(card.id)}
+                            key={index}
+                            card={card}
+                            enemy={false}
+                        />
+                    ))}
+                </div>
+                <div className={styles.mana}>
+                    <Image src="/mana.png" alt="mana" objectFit="contain" fill={true} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Player
