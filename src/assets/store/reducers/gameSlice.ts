4a21a83b3f4263c5c7f2e20b4a7f8e1c825b5976
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-12-06 22:56:02 +0200

    feat(gameSlice.ts): add update game status and updateTurn reducers

--- src/assets/store/reducers/gameSlice.ts
+++ src/assets/store/reducers/gameSlice.ts
@@ -1,38 +1,48 @@
-import { GAME_STATUS, GameType, TURN_STATUS } from "@/types/game.type";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { GAME_STATUS, GameType, TURN_STATUS } from '@/types/game.type'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 const initialState: GameType = {
-    status: GAME_STATUS.paused,
-    currentTurn: TURN_STATUS.player
+	status: GAME_STATUS.paused,
+	currentTurn: TURN_STATUS.player,
 }
 
 const gameSlice = createSlice({
-    name: 'game',
-    initialState,
-    reducers: {
-        startAction(state) {
-            state.status = GAME_STATUS.active
-        },
+	name: 'game',
+	initialState,
+	reducers: {
+		startAction(state) {
+			state.status = GAME_STATUS.active
+		},
 
-        pausedGame(state) {
-            const isPaused = state.status === GAME_STATUS.paused
-            state.status = isPaused ? GAME_STATUS.active : GAME_STATUS.paused;
-        },
+		pausedGame(state) {
+			const isPaused = state.status === GAME_STATUS.paused
+			state.status = isPaused ? GAME_STATUS.active : GAME_STATUS.paused
+		},
 
-        finishedGame(state) {
-            state.status = GAME_STATUS.finished;
-        },
+		finishedGame(state) {
+			state.status = GAME_STATUS.finished
+		},
 
-        nextTurn(state, action: PayloadAction<{turn: TURN_STATUS}>) {
-            state.currentTurn = action.payload.turn
-        }
-    },
+		nextTurn(state, action: PayloadAction<{ turn: TURN_STATUS }>) {
+			state.currentTurn = action.payload.turn
+		},
+
+		updateGameStatus(state, action: PayloadAction<{ status: GAME_STATUS }>) {
+			state.status = action.payload.status
+		},
+
+		updateTurn(state, action: PayloadAction<{ turn: TURN_STATUS }>) {
+			state.currentTurn = action.payload.turn
+		},
+	},
 })
 
-export const { 
-    startAction, 
-    pausedGame, 
-    finishedGame, 
-    nextTurn
+export const {
+	startAction,
+	pausedGame,
+	finishedGame,
+	nextTurn,
+	updateGameStatus,
+	updateTurn,
 } = gameSlice.actions
 export default gameSlice.reducer
